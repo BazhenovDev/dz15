e8d0bba02efd7c204fbc4944e3c936411f77a887
Ivan Bazhenov <bazhenov.dev@yandex.ru>
2024-10-30 23:34:30 +0300

    upload trash code

--- script.js
+++ script.js
@@ -19,12 +19,25 @@ Promise.all(
         persons = response[1];
         specializations = response[2];
 
-
         getInfo.call(persons[0]);
+        console.log('========================================')
+        console.log('Figma users');
         getUsersFigma();
+        console.log('========================================')
+        console.log('React user');
         getReactUser();
+        console.log('========================================')
+        console.log('Age users');
         fullAge();
-        backendFullDay();
+        console.log('========================================')
+        console.log('Backend full day users');
+        backendFullDay()
+        console.log('========================================')
+        console.log('Photoshop and figma users +6level')
+        getPhotoshopAndFigmaUsers()
+        console.log('========================================')
+        console.log('Dream Team')
+        dreamTeam()
 
     });
 
@@ -69,85 +82,191 @@ function getUsersFigma() {
     }
 }
 
-
-// Не понимаю, почему он тут возвращает всех юзеров, если я использую метод find, должен же возвращать первого попавшегося
 function getReactUser() {
-    const frontend = getUserSpecialization('frontend');
-    if (frontend && frontend.length > 0) {
-        const frontendReact = frontend.filter(skill => {
-            return getSkillUser(skill, 'react');
-        });
-        if (frontendReact.length > 0) {
-            frontendReact.find(user => {
-                return getInfo.call(user);
-            });
-        }
-    }
+    const reactUser = persons.find(user => {
+        return getSkillUser(user, 'react');
+    });
+    getInfo.call(reactUser);
 }
 
-// Код проверки на возраст, понимаю, что не совсем верно, т.к. нужно помимо года сверять ещё день и месяц, да и почему-то уверен, что в принципе код не верный
 function fullAge() {
-    const userYears = []
-    let yearToday = new Date().getFullYear();
-    persons.filter(person => {
-        const bDaySplit = person.personal.birthday.split('.');
-        const bDayYear = +bDaySplit[2]
-        return userYears.push(bDayYear)
+    let younger = persons.some(user => {
+        let birthdayParty = user.personal.birthday.split('.').reverse();
+        let adult = new Date(
+            +birthdayParty[0] + 18,
+            +birthdayParty[1] - 1,
+            +birthdayParty[2]
+        );
+        return new Date() < adult;
     });
-    let getError = false;
-    userYears.findIndex(year => {
-        if ((yearToday - year) < 18) {
-            return getError = true;
-        } else {
-            return getError = false;
-        }
+    console.log(!younger ? 'Все старше 18' : 'Есть несовершеннолетние');
+}
+
+// function userLocation(user, locName) {
+//     const location = cities.find(city => city.name.toLowerCase() === locName);
+//     if (location && location.name) {
+//         user = persons;
+//     }
+//     return user.filter(user => {
+//         const userLocation = cities.find(city => city.id === user.personal.locationId);
+//         return userLocation && userLocation.name.toLowerCase() === locName;
+//     });
+// }
+
+function userLocation(userArray, locName) {
+    const loc = cities.find(city => {
+        return city.name.toLowerCase() === locName;
     });
-    if (getError) {
-        console.log('Есть несовершеннолетние');
-    } else {
-        console.log('Все совершеннолетние');
+    if (loc && loc.name) {
+        return userArray.personal.locationId === loc.id;
     }
 }
 
+function workSchedule(user, typeVal) {
+    const typeName = user.request.find(item => {
+        return item.name.toLowerCase() === 'тип занятости';
+    });
+    if (typeName && typeName.name) {
+        return typeName.value.toLowerCase() === typeVal;
+    }
+}
 
-// function location(userLoc) {
-//     const userCity = cities.find(city => {
-//         return city.name.toLowerCase() === userLoc;
+// function sortSalary(userArray) {
+//     const salary = userArray.request.find(reqItem => {
+//         return reqItem.name.toLowerCase() === 'зарплата'
 //     });
-//     if (userCity && userCity.id) {
-//         return persons.filter(person => {
-//             return person.personal.locationId === userCity.id;
+//     if (salary && salary.name) {
+//         salary.sort((a,b) => {
+//             return a.value - b.value
 //         });
 //     }
 // }
 
 
-function userLocation(user, locName) {
-    const loc = cities.find(city => {
-        return city.name.toLowerCase() === locName;
+// function sortSalary(userArray) {
+//     return userArray.filter(user => {
+//         return user.request.find(req => {
+//             let zp = req.name.toLowerCase() === 'зарплата'
+//             console.log(zp)
+//         })
+//
+//         // if (salary && salary.value) {
+//         //    return userArray.salary.sort((a,b) => {
+//         //        return a.value - b.value
+//         //    })
+//         // }
+//     });
+// }
+
+// function sortSalary(userArray) {
+//     let salary = userArray.filter(user => {
+//        return user.request.find(req => {
+//             return req.name.toLowerCase() === 'зарплата'
+//         });
+//
+//     });
+//     console.log(salary)
+// }
+
+function sortSalary(userArray) {
+    return userArray.sort((a, b) => {
+        return a.request[0].value - b.request[0].value
     });
-    if (loc && loc.name) {
-        return user.personal.locationId === loc.id;
-    }
 }
 
 function backendFullDay() {
-    // const moscowUsers = userLocation('москва');
     const backendUsers = getUserSpecialization('backend')
-    if (backendUsers && backendUsers.length > 0) {
-        const backendMoscow = backendUsers.filter(user => {
-            return userLocation(user, 'москва');
+    if (backendUsers.length > 0) {
+        const usersFromMoscow = backendUsers.filter(user => {
+            return userLocation(user, 'москва')
         });
-        console.log(backendMoscow)
+        if (usersFromMoscow.length > 0) {
+            const usersWorksFullDay = usersFromMoscow.filter(user => {
+                return workSchedule(user, 'полная')
+            });
+            let sortArray = sortSalary(usersWorksFullDay);
+            sortArray.forEach(user => {
+                getInfo.call(user)
+            });
+        }
     }
-    // console.log(backendUsers)
 }
 
+function getPhotoshopAndFigmaUsers() {
+    const usersPhotoshopAndFigma = persons.filter(user => {
+        const photoshopUsers = getSkillUser(user, 'photoshop');
+        const figmaUsers = getSkillUser(user, 'figma');
+        if ((photoshopUsers && photoshopUsers.name) && (figmaUsers && figmaUsers.name) && (photoshopUsers.level >= 6 && figmaUsers.level >= 6)) {
+            return user;
+        }
+    });
+    usersPhotoshopAndFigma.forEach(user => {
+        getInfo.call(user);
+    });
+}
 
+function dreamTeam() {
+    const team = persons.filter(user => {
+        const figmaUsers = getSkillUser(user, 'figma');
+        const angularUsers = getSkillUser(user, 'angular');
+        const goUsers = getSkillUser(user, 'go');
+        if (figmaUsers && figmaUsers.name) {
+            return user
+        }
 
+        if (angularUsers && angularUsers.name) {
+            // return user
+        }
+        if (goUsers && goUsers.name) {
+            // return user
+        }
+    });
+    console.log(team);
 
-
-
+    // let maxLevel = 0;
+    // let maxLevelFigma = null;
+    // let maxLevelAngular = null;
+    // let maxLevelGo = null;
+    //
+    // persons.forEach(user => {
+    //     const angularUser = user.skills.find(skill => {
+    //         return skill.name.toLowerCase() === 'angular'
+    //     })
+    //     if (angularUser && angularUser.name) {
+    //         const angularNum = parseInt(angularUser.level)
+    //         if (!isNaN(angularNum) && angularNum > maxLevel) {
+    //             maxLevel = angularNum
+    //             maxLevelAngular = user
+    //         }
+    //     }
+    //
+    //     const figmaUser = user.skills.find(skill => {
+    //         return skill.name.toLowerCase() === 'figma'
+    //     })
+    //     if (figmaUser && figmaUser.name) {
+    //         const figmaNum = parseInt(figmaUser.level)
+    //         if (!isNaN(figmaNum) && figmaNum > maxLevel) {
+    //             maxLevel = figmaNum
+    //             maxLevelFigma = user
+    //         }
+    //     }
+    //
+    //     const goUser = user.skills.find(skill => {
+    //         return skill.name.toLowerCase() === 'go'
+    //     })
+    //     if (goUser && goUser.name) {
+    //         const goNum = parseInt(goUser.level)
+    //         if (!isNaN(goNum) && goNum > maxLevel) {
+    //             maxLevel = goNum
+    //             maxLevelGo = user
+    //         }
+    //     }
+    // })
+    //
+    // console.log(maxLevelFigma)
+    // console.log(maxLevelAngular)
+    // console.log(maxLevelGo)
+}
 
 
 
